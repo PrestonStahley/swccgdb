1988bd2bd40d318b855b34417fc0862c9ddaca6b
PrestonStahley <preston.stahley@gmail.com>
2018-06-02 17:47:52 -0600

    Render all card types in decklist view

--- src/AppBundle/Resources/public/js/app.deck.js
+++ src/AppBundle/Resources/public/js/app.deck.js
@@ -28,7 +28,7 @@
      * Templates for the different deck layouts, see deck.get_layout_data
      */
     layouts[1] = _.template('<div class="deck-content"><%= meta %><%= plots %><%= characters %><%= attachments %><%= locations %><%= events %></div>');
-    layouts[2] = _.template('<div class="deck-content"><div class="row"><div class="col-sm-5 col-print-6"><%= images %></div><div class="col-sm-7 col-print-6"><%= meta %></div></div><div class="row"><div class="col-sm-6 col-print-6"><%= characters %></div><div class="col-sm-6 col-print-6"><%= attachments %><%= locations %><%= events %></div></div></div>');
+    layouts[2] = _.template('<div class="deck-content"><div class="row"><div class="col-sm-5 col-print-6"><%= images %></div><div class="col-sm-7 col-print-6"><%= meta %></div></div><div class="row"><div class="col-sm-6 col-print-6"><%= locations %><%= characters %><%= starships %><%= creatures %></div><div class="col-sm-6 col-print-6"><%= weapons %><%= devices %><%= admirals-orders %><%= epic-events %><%= jedi-tests %><%= podracers %><%= effects %><%= interrupts %><%= defensive-shields %></div></div></div>');
     layouts[3] = _.template('<div class="deck-content"><div class="row"><div class="col-sm-4"><%= meta %><%= plots %></div><div class="col-sm-4"><%= characters %></div><div class="col-sm-4"><%= attachments %><%= locations %><%= events %></div></div></div>');
 
     /**
